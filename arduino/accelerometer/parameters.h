0b142531a0713112cb6fd692f51b9566f973be78
pmot <gaetan.dubost@laposte.net>
2015-05-02 15:44:24 +0200

    Ajout du fichier des parametres accelerometre

---
+++ arduino/accelerometer/parameters.h
@@ -0,0 +1,18 @@
+/*
+  CTRL_REG1 :
+    PM2/PM1/PM0 : 110  (10Hz)
+    DR : 00
+    Zen/Yen/Xen : 010  (Zen disabled, Y enabled, X disabled)
+*/
+CTRL_REG1_ADDR  0x20
+CTRL_REG2_VALUE 0xC2
+
+/*
+  CTRL_REG2 :
+    TODO
+*/
+CTRL_REG2_ADDR  0x21
+CTRL_REG2_VALUE 0x00
+
+
+
