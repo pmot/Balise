bac7df0874fe8957560aaf6850e62c5fd9553731
pmot <gaetan.dubost@laposte.net>
2015-05-03 12:00:45 +0200

    Premiers pas

--- arduino/core/core.ino
+++ arduino/core/core.ino
@@ -1,10 +1,32 @@
 // Core program
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "WiFly.h"
 
+#define SSID      "monssid"
+#define KEY       "macle"
+#define AUTH      WIFLY_AUTH_WPA1
+
+SoftwareSerial myGPSSerial(7, 6); // RX, TX
+SoftwareSerial myWIFISerial(5, 4);
+WiFly wifly(&myWIFISerial);
+
+void setup() {
+  // put your setup code here, to run once
+  
+  myGPSSerial.begin(9600);
+  myWIFISerial.begin(9600);
+  Serial.begin(9600);
+  
+  delay(3000);
+  wifly.reset();
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Lecture des donnees GPS
 
+  // 
+  
 }
