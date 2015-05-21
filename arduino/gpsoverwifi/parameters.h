17bc922b072f2d681c81a1e44e20e00a254145d8
pmot <gaetan.dubost@laposte.net>
2015-05-21 22:05:16 +0200

    Ajout de la collecte de données GPS et affichage sympa. Première version, les données sont envoyées sur le port série.

---
+++ arduino/gpsoverwifi/parameters.h
@@ -0,0 +1,15 @@
+
+
+// #define SSID      "biscotte"
+// #define KEY       "marechaldeslogischef"
+// #define AUTH      WIFLY_AUTH_WPA1
+// #define AUTH      WIFLY_AUTH_WPA2_PSK
+#define SSID      "kim"
+#define KEY       ""
+#define AUTH      WIFLY_AUTH_OPEN
+#define WIFI_TX  4
+#define WIFI_RX  5
+#define GPS_TX   6
+#define GPS_RX   7
+
+
