eb025b9f0e8b4602e4f74b9130028fd3040d1996
pmot <gaetan.dubost@laposte.net>
2015-05-31 22:10:54 +0200

    Le build fonctionne à présent.

--- build/build.sh
+++ build/build.sh
@@ -5,15 +5,13 @@ test -r ./config && source ./config || PLATFORM="ARDUINO"
 case x${PLATFORM} in
   xARDUINO)
 	source ./config.${PLATFORM}
-	pushd ${ARDUINO_DIR}
+	pushd ${PLATFORM_DIR}
 	echo "Building sketch..."
 	# TODO...
 	echo "Uploading sketch..."
 	make upload
+	popd
 	;;
-
   *) 	source ./config.${PLATFORM}
 	;;
 esac
-
-popd
