4daac7dfdb0d819dc7176d8829475a86d59e6b74
pmot <gaetan.dubost@laposte.net>
2015-06-02 22:37:16 +0200

    Effet visuel

--- arduino/core/core.ino
+++ arduino/core/core.ino
@@ -3,17 +3,16 @@
 #include <SoftwareSerial.h>
 #include <WiFly.h>
 
+// the setup function runs once when you press reset or power the board
 void setup() {
-
-  Serial.begin(9600);
-  
-  delay(3000);
-  
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // Lecture des donnees GPS
-
-  // 
-  
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
 }
