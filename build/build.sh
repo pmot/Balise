5d5eb9f5ead6e192f358866594b5e10586f11904
pmot <gaetan.dubost@laposte.net>
2015-06-03 22:48:56 +0200

    Suppression d'un fichier swap résiduel. Le script build.sh tente l'upload que si un port ttyUSBX est disponible.

--- build/build.sh
+++ build/build.sh
@@ -6,10 +6,13 @@ case x${PLATFORM} in
   xARDUINO)
 	source ./config.${PLATFORM}
 	pushd ${PLATFORM_DIR}
-	echo "Building sketch..."
-	# TODO...
-	echo "Uploading sketch..."
-	make upload
+	echo "Compilation de core.ino..."
+	make && echo "Projet compilé dans ${HOME}/BaliseBuild" \
+		|| { echo "La compilation a échoué"; exit 1; }
+	ls -1 /dev/ttyUSB* 2>&1 > /dev/null && { \
+		echo "Téléversement de l'image..." ; \
+		make upload ; \
+	}
 	popd
 	;;
   *) 	source ./config.${PLATFORM}
