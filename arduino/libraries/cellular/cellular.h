1e08ad918344b6a2eb8945a3fec5f4cd433d8388
pmot <gaetan.dubost@laposte.net>
2015-06-21 20:13:39 +0200

    Build.

--- arduino/libraries/cellular/cellular.h
+++ arduino/libraries/cellular/cellular.h
@@ -1,8 +1,12 @@
 #ifndef __CELLULAR_H__
 #define __CELLULAR_H__
 
+enum protocols{ CCEU_HTTP, CCEU_SIP, CCEU_RAW_UDP};
+
 #include <Arduino.h>
 
+int makeData(char*, enum protocols); // DATA, Protocole
 int post(char*, char*, char*);  // Host, URL, DATA
+int send(char*, int, enum protocols); // Host, port, proto (CCEU_SIP ou CCEU_RAW_UDP)
 
 #endif
--- build/build.sh
+++ build/build.sh
@@ -2,17 +2,26 @@
 
 test -r ./config && source ./config || PLATFORM="ARDUINO"
 
+for flag in DEBUG WIFI_SCAN GPS ACCELEROMETER CELLULAR_SMS CELLULAR_DATA
+do
+  if [ -n ${flag} ]
+  then
+    FLAGS=" -D${flag} ${C_FLAGS}" 
+  fi
+done
+echo "${C_FLAGS}"
+
 case x${PLATFORM} in
   xARDUINO)
 	source ./config.${PLATFORM}
 	pushd ${PLATFORM_DIR}
 	echo "Compilation de core.ino..."
-	make && echo "Projet compilé dans ${HOME}/BaliseBuild" \
+	C_FLAG=${FLAGS} make && echo "Projet compilé dans ${HOME}/BaliseBuild" \
 		|| { echo "La compilation a échoué"; exit 1; }
 
 	ls -1 /dev/ttyUSB* 2>&1 > /dev/null && { \
 		echo "Téléversement de l'image..." ; \
-		make upload ; \
+		C_FLAG=${FLAGS} make upload ; \
 	} || echo "Aucun périphérique Arduino connecté"
 	popd
 	;;
