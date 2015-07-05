202947f6387c40e00761711652866f6b606d282f
Christophe DUPLAIX <cduplaix@free.fr>
2015-07-05 22:07:08 +0200

    NC

--- arduino/libraries/accelerometer/accelerometer.cpp
+++ arduino/libraries/accelerometer/accelerometer.cpp
@@ -6,10 +6,10 @@ void accelerometerSetup(LIS331 lis) {
 	// write to Control register 1: address 20h
 	byte addressByte = 0x20;
 	/* Bits:
-PM2 PM1 PM0 DR1 DR0 Zen Yen Xen
-PM2PM1PM0: Power mode (001 = Normal Mode)
-DR1DR0: Data rate (00=50Hz, 01=100Hz, 10=400Hz, 11=1000Hz)
-Zen, Yen, Xen: Z enable, Y enable, X enable
+			PM2 PM1 PM0 DR1 DR0 Zen Yen Xen
+			PM2PM1PM0: Power mode (001 = Normal Mode)
+			DR1DR0: Data rate (00=50Hz, 01=100Hz, 10=400Hz, 11=1000Hz)
+			Zen, Yen, Xen: Z enable, Y enable, X enable
 	 */
 	byte ctrlRegByte = 0x3A; // 00111010 : normal mode, 1000Hz, y enabled, xz disabled
 	// Send the data for Control Register 1
@@ -25,20 +25,20 @@ Zen, Yen, Xen: Z enable, Y enable, X enable
 	// Control Register 3 configures Interrupts
 	addressByte = 0x22;
 	/* Bits:
-IHL PP_OD LIR2 I2_CFG1 I2_CFG0 LIR1 I1_CFG1 I1_CFG0
-IHL: interrupt high low (0=active high)
-PP_OD: push-pull/open drain selection (0=push-pull)
-LIR2: latch interrupt request on INT2_SRC register (0=interrupt request not latched)
-I2_CFG1, I2_CFG0: data signal on INT 2 pad control bits
-LIR1: latch interrupt request on INT1_SRC register (0=interrupt request not latched)
-I1_CFG1, I1_CFG0: data signal on INT 1 pad control bits
+		IHL PP_OD LIR2 I2_CFG1 I2_CFG0 LIR1 I1_CFG1 I1_CFG0
+		IHL: interrupt high low (0=active high)
+		PP_OD: push-pull/open drain selection (0=push-pull)
+		LIR2: latch interrupt request on INT2_SRC register (0=interrupt request not latched)
+		I2_CFG1, I2_CFG0: data signal on INT 2 pad control bits
+		LIR1: latch interrupt request on INT1_SRC register (0=interrupt request not latched)
+		I1_CFG1, I1_CFG0: data signal on INT 1 pad control bits
 
-Data signal on pad
-I1(2)_CFG1      I1(2)_CFG0            INT 1(2) pad
-   0                0              interrupt 1(2) source
-   0                1              interrupt 1 source or interrupt 2 source
-   1                0              data ready
-   1                1              boot running
+		Data signal on pad
+		I1(2)_CFG1      I1(2)_CFG0            INT 1(2) pad
+		   0                0              interrupt 1(2) source
+		   0                1              interrupt 1 source or interrupt 2 source
+		   1                0              data ready
+		   1                1              boot running
 
 	 */
 	ctrlRegByte = 0x00; // 00000000 : default
@@ -47,13 +47,13 @@ I1(2)_CFG1      I1(2)_CFG0            INT 1(2) pad
 	// write to Control Register 4: address 23h
 	addressByte = 0x23;
 	/* Bits:
-BDU BLE FS1 FS0 STsign 0 ST SIM
-BDU: Block data update (0=continuous update)
-BLE: Big/little endian data (0=accel data LSB at LOW address)
-FS1FS0: Full-scale selection (00 = +/-6G, 01 = +/-12G, 11 = +/-24G)
-STsign: selft-test sign (default 0=plus)
-ST: self-test enable (default 0=disabled)
-SIM: SPI mode selection(default 0=4 wire interface, 1=3 wire interface)
+		BDU BLE FS1 FS0 STsign 0 ST SIM
+		BDU: Block data update (0=continuous update)
+		BLE: Big/little endian data (0=accel data LSB at LOW address)
+		FS1FS0: Full-scale selection (00 = +/-6G, 01 = +/-12G, 11 = +/-24G)
+		STsign: selft-test sign (default 0=plus)
+		ST: self-test enable (default 0=disabled)
+		SIM: SPI mode selection(default 0=4 wire interface, 1=3 wire interface)
 	 */
 	ctrlRegByte = 0x30; // 00110000 : 24G (full scale)
 	lis.writeReg(addressByte, ctrlRegByte);
