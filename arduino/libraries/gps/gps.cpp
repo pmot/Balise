c6544c6df66018747ee0d000c3905b7a9dfde6ac
Christophe DUPLAIX <cduplaix@free.fr>
2015-07-05 22:08:29 +0200

    NC

--- arduino/libraries/gps/gps.cpp
+++ arduino/libraries/gps/gps.cpp
@@ -2,11 +2,12 @@
 
 void gpsRead(TinyGPS gps, SoftwareSerial serial, unsigned long ms)
 {
-  serial.listen();
-  unsigned long start = millis();
-  do 
-  {
-    while (serial.available())
-      gps.encode(serial.read());
-  } while (millis() - start < ms);
+	serial.listen();
+
+	unsigned long start = millis();
+
+	do {
+		while (serial.available())
+			gps.encode(serial.read());
+	} while (millis() - start < ms);
 }
