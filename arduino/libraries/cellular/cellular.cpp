9d0ea8b71d1959102c2b212e3cc4c3f59220fb8e
pmot <gaetan.dubost@laposte.net>
2015-07-08 22:29:58 +0200

    Modification de la méthode de setup de la librairie interne cellular

--- arduino/libraries/cellular/cellular.cpp
+++ arduino/libraries/cellular/cellular.cpp
@@ -1,1 +1,13 @@
 #include "cellular.h"
+
+bool cellSetup(HardwareSerial mySerial)
+{
+	mySerial.begin(CELL_SERIAL_BAUDRATE);
+	return true;
+	// Deal with autobauding...
+	mySerial.print("A");
+	delay(1000);
+	mySerial.print("T");
+	delay(1000);
+	mySerial.print("\r");
+}
--- arduino/libraries/cellular/cellular.h
+++ arduino/libraries/cellular/cellular.h
@@ -1,12 +1,16 @@
 #ifndef __CELLULAR_H__
 #define __CELLULAR_H__
 
-enum protocols{ CCEU_HTTP, CCEU_SIP, CCEU_RAW_UDP};
+#define CELL_SERIAL_BAUDRATE	115200
+
+enum protocols{ CCEU_HTTP, CCEU_SIP, CCEU_RAW_UDP, GEOLOC_HTTP };
 
 #include <Arduino.h>
 
-int makeData(char*, enum protocols); // DATA, Protocole
-int post(char*, char*, char*);  // Host, URL, DATA
-int send(char*, int, enum protocols); // Host, port, proto (CCEU_SIP ou CCEU_RAW_UDP)
-
+// Initialise le port COM et envoie un AT 
+bool cellSetup(HardwareSerial);
+int cellMakeData(char*, enum protocols); // DATA, Protocole
+int cellPost(char*, char*, char*);  // Host, URL, DATA
+int cellSend(char*, int, enum protocols); // Host, port, protocole
+int cellExpect(char*, int);	// Attend une chaîne en retour
 #endif
